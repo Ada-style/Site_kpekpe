93200ef29d999725d2bf5143e07d0a20ff4c23ff
Ada-style <adadossi24@gmail.com>
2026-02-25 19:29:36 +0000

    Déploiement Ready : ALLOWED_HOSTS=['*'], SECRET_KEY env, Dockerfile optimisé

--- kpekpe-backend/kpekpe_backend/settings.py
+++ kpekpe-backend/kpekpe_backend/settings.py
@@ -10,22 +10,19 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x0l-1&dn4l&1t0=#pq-&2*l-fii23799rm#os893j7gc$(wb9m'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-x0l-1&dn4l&1t0=#pq-&2*l-fii23799rm#os893j7gc$(wb9m')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
